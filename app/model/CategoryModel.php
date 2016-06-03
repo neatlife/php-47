27de7eda9ecdbab59c7dfa23a4f15a656b5b1087
suxiaolin <suxiaolin@yandex.com>
2016-06-03 14:38:33 +0800

    将重要文件打上骷髅标记

--- app/model/CategoryModel.php
+++ app/model/CategoryModel.php
@@ -35,7 +35,10 @@ class CategoryModel extends Model
     // 连接查询出所有的分类
     public function getAllWithJoin()
     {
-        $sql = "SELECT category.*, count(article.id) AS count FROM category LEFT JOIN article ON category.id=article.category_id GROUP BY category.id";
+        $sql = "SELECT category.*, count(article.id) AS count
+                  FROM category
+                  LEFT JOIN article ON category.id=article.category_id
+                  GROUP BY category.id";
         return $this->getAll($sql);
     }
 }
