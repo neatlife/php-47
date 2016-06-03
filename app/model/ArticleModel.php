9dd6d0e0bc9c4277680d3579e2403cb6805c0320
suxiaolin <suxiaolin@yandex.com>
2016-06-03 11:32:28 +0800

    11.文章表(article)连接用户表(user)查询出文章的作者的名字(user.username)的代码实现

--- app/model/ArticleModel.php
+++ app/model/ArticleModel.php
@@ -16,9 +16,10 @@ class ArticleModel extends \core\Model
 
     public function getAllWithJoin($where = '2 > 1', $sort = 'id ASC', $start = 0, $pageSize = 10)
     {
-        $sql = "SELECT article.*, category.name AS category_name
+        $sql = "SELECT article.*, category.name AS category_name, user.username
                   FROM article
                   LEFT JOIN category ON article.category_id=category.id
+                  LEFT JOIN user ON article.author_id=user.id
                   WHERE {$where}
                   ORDER BY {$sort}
                   LIMIT {$start}, {$pageSize}";
