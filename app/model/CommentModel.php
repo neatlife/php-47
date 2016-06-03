b9c7fc57876e3dcb203c1f4c56aa38a1e067e615
suxiaolin <suxiaolin@yandex.com>
2016-06-03 11:58:03 +0800

    12.创建评论表对应的模型

---
+++ app/model/CommentModel.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * CommentModel.php
+ * Created by day11.
+ * User: 苏小林
+ * Date: 2016/6/3
+ * Time: 11:55
+ */
+
+namespace app\model;
+
+use core\Model;
+
+class CommentModel extends Model
+{
+    protected $table = 'comment';
+}
