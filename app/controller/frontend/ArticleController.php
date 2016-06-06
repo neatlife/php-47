2d9b2209086ccd5a91512a0ca776c8ecf7b19f47
suxiaolin <suxiaolin@yandex.com>
2016-06-06 17:34:12 +0800

    在博客列表里显示无限极分类

--- app/controller/frontend/ArticleController.php
+++ app/controller/frontend/ArticleController.php
@@ -11,6 +11,7 @@ namespace app\controller\frontend;
 
 
 use app\model\ArticleModel;
+use app\model\CategoryModel;
 use core\Controller;
 
 class ArticleController extends Controller
@@ -18,8 +19,12 @@ class ArticleController extends Controller
     public function getList()
     {
         $articles = ArticleModel::create()->getAllWithJoin('2 > 1', '`id` ASC', 0, false);
+        $categories = CategoryModel::create()->limitlessLevelCategory(
+            CategoryModel::create()->findAll()
+        );
         $this->s->assign(array(
             'articles' => $articles,
+            'categories' => $categories,
         ));
         $this->s->display('frontend/article/getList.html');
     }
