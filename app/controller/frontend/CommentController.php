65c2d8fc0f07a5b4eea1da5737fe3652048231fd
suxiaolin <suxiaolin@yandex.com>
2016-06-07 14:54:45 +0800

    创建评论控制器(app controller frontend CommetController)，定义评论控制器的添加评论(add)方法

---
+++ app/controller/frontend/CommentController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * CommentController.php
+ * Created by day11.
+ * User: 苏小林
+ * Date: 2016/6/7
+ * Time: 14:53
+ */
+
+namespace app\controller\frontend;
+
+
+use core\Controller;
+
+class CommentController extends Controller
+{
+    public function add()
+    {
+
+    }
+}
