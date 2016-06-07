9b03ec823222034453009c7ce239464a2301318f
suxiaolin <suxiaolin@yandex.com>
2016-06-07 15:16:28 +0800

    使用CommentModel将收集的评论表的字段值插入评论表中

--- app/controller/frontend/CommentController.php
+++ app/controller/frontend/CommentController.php
@@ -10,12 +10,35 @@
 namespace app\controller\frontend;
 
 
+use app\model\CommentModel;
 use core\Controller;
 
 class CommentController extends Controller
 {
     public function add()
     {
+        $this->denyAccess();
+        $userId = $_SESSION['user']['id'];
+        $articleId = $_GET['article_id'];
+        $parentId = $_POST['inpRevID'];
+        $content = $_POST['txaArticle'];
+        $publishTime = time();
 
+        if (CommentModel::create()->add(array(
+            'user_id' => $userId,
+            'article_id' => $articleId,
+            'parent_id' => $parentId,
+            'content' => $content,
+            'publish_time' => $publishTime,
+        ))) {
+            // 添加评论成功
+            // 返回p=frontend c=Article a=detail id=$articleId
+            $this->redirect("index.php?p=frontend&c=Article&a=detail&id={$articleId}", "添加评论成功");
+        } else {
+            // 添加评论失败
+            // 返回p=frontend c=Article a=detail id=$articleId
+            $this->redirect("index.php?p=frontend&c=Article&a=detail&id={$articleId}", "添加评论失败");
+        }
+        // var_dump($articleId, $_POST);die;
     }
 }
